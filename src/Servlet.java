9f5ff73edd00bc1adb24a40effdd1a6e90c25172
rosa <gojiradam@gmail.com>
2016-02-17 21:05:50 +0200

    Initial commit

---
+++ src/Servlet.java
@@ -0,0 +1,38 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by rosa on 2/17/16.
+ */
+@WebServlet(name = "Servlet")
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+        try (PrintWriter writer = response.getWriter()) {
+
+            writer.println("<!DOCTYPE html><html>");
+            writer.println("<head>");
+            writer.println("<meta charset=\"UTF-8\" />");
+            writer.println("<title>MyServlet.java:doGet(): Servlet code!</title>");
+            writer.println("</head>");
+            writer.println("<body>");
+
+            writer.println("<h1>This is a simple java servlet.</h1>");
+
+            writer.println("</body>");
+            writer.println("</html>");
+
+        }
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: rosa
+  Date: 2/17/16
+  Time: 8:44 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <h1>Simple Java Web App Demo</h1>
+  <p>To invoke the java servlet click <a href="MyServlet">here</a></p>
+  </body>
+</html>
